6877b80870ab3d7a50f5c3e4abb6ed6ed7359004
Lonozx <lonozx@gmail.com>
2023-02-14 00:18:00 +0200

    Commit header

---
+++ test-thing/src/common/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './footer.scss'
+function Footer(props){
+    return (
+        <div className='footer'>
+            <p className='footer__desc'>You beat it</p>
+        </div>
+    )
+}
+
+export default Footer
